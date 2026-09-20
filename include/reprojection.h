e8a4b569a7e9af2fc2fa3ae28b0fd9c4ff398128
liqianchuan <liqianchuan32@gmail.com>
2026-09-20 14:22:27 +0800

    fix: repair Camera struct and Mat3 initialization

--- include/reprojection.h
+++ include/reprojection.h
@@ -8,30 +8,25 @@ struct Vec3{
 };
 //三维世界下的三维点，相机坐标系下的三维点，还有平移向量
 struct Mat3{
-    double m[3][3];
-    static Mat3 idendity(){//标准旋转矩阵
-        Mat3 R;
+    double m[3][3]{};
+    static Mat3 idendity()
+    {//标准旋转矩阵
+        Mat3 R{};
         R.m[0][0]=1.0;
         R.m[1][1]=1.0;
         R.m[2][2]=1.0;
         return R;
-    }
+    };
 };
 Vec3 operator*(const Mat3& R, const Vec3& p);//矩阵乘向量
 Vec3 operator+(const Vec3& a, const Vec3& b);//矩阵加法
 //Mat3为相机的朝向
 struct Camera{
     double fx,fy,cx,cy;
-    Mat3 R=Mat3::idendity(){
-        Mat3 R{};
-        R.m[0][0]=1.0;
-        R.m[1][1]=1.0;
-        R.m[2][2]=1.0;
-        return R;
-    }；
+    Mat3 R=Mat3::idendity();
+    Vec3 t{};//相机平移向量
 };//相机旋转矩阵
-    Vec3 t;//相机平移向量
-};//fx fy为像素，cx cy为主点像素坐标
+//fx fy为像素，cx cy为主点像素坐标
 struct reprojectionResult{
      bool valid = false;//判断是否有效（在不在相机后面）
     Vec2 pixel{};//返回的像素点坐标
